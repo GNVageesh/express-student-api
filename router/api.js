299a066d4c84b11eec804c81df4e5d1a24ebfdd8
GNVageesh <vageeshgn2005@gmail.com>
2021-08-12 11:04:47 +0530

    env vars

--- router/api.js
+++ router/api.js
@@ -2,12 +2,18 @@ const express = require("express");
 const router = express.Router();
 const Student = require("../model/student");
 
-router.get("/students", (req, res, next) => {
-	Student.find({})
-		.then((students) => {
-			res.send(students);
-		})
-		.catch(next);
+router.get("/students", async (req, res, next) => {
+	// Student.find({})
+	// 	.then((students) => {
+	// 		res.send(students);
+	// 	})
+	// 	.catch(next);
+	try {
+		const data = await Student.find();
+		res.json(data);
+	} catch {
+		res.send({ message: "Some error occured" });
+	}
 });
 router.get("/students/:id", async (req, res, next) => {
 	try {
@@ -18,12 +24,24 @@ router.get("/students/:id", async (req, res, next) => {
 	}
 });
 
-router.post("/students", (req, res, next) => {
-	Student.create(req.body)
-		.then((student) => {
-			res.send(student);
-		})
-		.catch(next);
+router.post("/students", async (req, res, next) => {
+	// Student.create(req.body)
+	// 	.then((student) => {
+	// 		res.send(student);
+	// 	})
+	// 	.catch(next);
+
+	try {
+		const structure = {
+			name: req.body.name,
+			roll: req.body.roll,
+			present: req.body.present,
+		};
+		const data = await Student.create(structure);
+		res.json(data);
+	} catch (err) {
+		res.send({ message: err.message });
+	}
 });
 
 router.put("/students/:id", (req, res, next) => {
@@ -36,12 +54,22 @@ router.put("/students/:id", (req, res, next) => {
 	);
 });
 
-router.delete("/students/:id", (req, res) => {
-	Student.findOneAndDelete({ _id: req.params.id }).then((student) => {
+router.delete("/students/:id", async (req, res) => {
+	// Student.findOneAndDelete({ _id: req.params.id }).then((student) => {
+	// 	res.send({
+	// 		message: "Deleted " + req.body.name + " from the database",
+	// 	});
+	// });
+
+	try {
+		const data = await Student.findOneAndDelete({ _id: req.params.id });
 		res.send({
-			message: "Deleted " + req.body.name + " from the database",
+			status: "removed from the database",
+			data,
 		});
-	});
+	} catch (err) {
+		res.json(err);
+	}
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
+	useFindAndModify: false,
 });
 mongoose.Promise = global.Promise;
 mongoose.connection.on("open", () => {
